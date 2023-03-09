e5d772435aa4cbd742b03b35930b09b5a4397a96
Maribelll <48179911+Maribelll@users.noreply.github.com>
2023-03-09 17:13:09 -0500

    add:start1

---
+++ .next/server/chunks/393.js
@@ -0,0 +1,186 @@
+exports.id = 393;
+exports.ids = [393];
+exports.modules = {
+
+/***/ 7337:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"footer": "footer_footer__CKQ67",
+	"footermain": "footer_footermain__APDQh",
+	"menufooter": "footer_menufooter__0PfeU",
+	"contactfooter": "footer_contactfooter__OPZxi",
+	"email": "footer_email__bk6Bf",
+	"form": "footer_form__dVfm3",
+	"follow": "footer_follow__7er_m",
+	"insta": "footer_insta___D0oQ",
+	"logofooter": "footer_logofooter__bjfbI",
+	"privacy": "footer_privacy__N4Knm"
+};
+
+
+/***/ }),
+
+/***/ 3899:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"header": "navadapt_header__1iLS_",
+	"menu": "navadapt_menu__SrSmi",
+	"navLi": "navadapt_navLi__zj7_E",
+	"menuBtn": "navadapt_menuBtn__OWMYl",
+	"menuIcon": "navadapt_menuIcon__8SB3N",
+	"navicon": "navadapt_navicon__kNtXH",
+	"steps": "navadapt_steps__sI7BQ",
+	"logo": "navadapt_logo__asSEN"
+};
+
+
+/***/ }),
+
+/***/ 5031:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"navbar": "navigation_navbar__QOsZm",
+	"menu": "navigation_menu__wXBh_"
+};
+
+
+/***/ }),
+
+/***/ 558:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "$": () => (/* binding */ Footer)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(7337);
+/* harmony import */ var _styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1__);
+
+
+function Footer() {
+    return(/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("footer", {
+            className: (_styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1___default().footer),
+            children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: (_styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1___default().footermain),
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1___default().form),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                                children: "Join the list"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                                className: (_styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1___default().logofooter)
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        className: (_styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1___default().contactfooter),
+                        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                            className: (_styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1___default().follow),
+                            children: [
+                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                                    children: "Follow along"
+                                }),
+                                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                                    className: (_styles_footer_module_scss__WEBPACK_IMPORTED_MODULE_1___default().social)
+                                })
+                            ]
+                        })
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                            children: "Company"
+                        })
+                    })
+                ]
+            })
+        })
+    }));
+}
+
+
+/***/ }),
+
+/***/ 6284:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "w": () => (/* binding */ Navbar)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: ./node_modules/next/link.js
+var next_link = __webpack_require__(1664);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(5675);
+// EXTERNAL MODULE: ./styles/navigation.module.scss
+var navigation_module = __webpack_require__(5031);
+var navigation_module_default = /*#__PURE__*/__webpack_require__.n(navigation_module);
+// EXTERNAL MODULE: ./styles/navadapt.module.scss
+var navadapt_module = __webpack_require__(3899);
+var navadapt_module_default = /*#__PURE__*/__webpack_require__.n(navadapt_module);
+;// CONCATENATED MODULE: ./Components/Navadapt.js
+
+
+function Navadapt() {
+    return(/*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx("header", {
+            className: (navadapt_module_default()).header
+        })
+    }));
+}
+
+;// CONCATENATED MODULE: ./Components/Navbar.js
+
+
+
+
+
+
+function Navbar() {
+    return(/*#__PURE__*/ (0,jsx_runtime_.jsxs)("nav", {
+        className: (navigation_module_default()).navbar,
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {}),
+            /*#__PURE__*/ jsx_runtime_.jsx(Navadapt, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {}),
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                className: (navigation_module_default()).btnMenu,
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                        className: (navigation_module_default()).line1
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                        className: (navigation_module_default()).line2
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                        className: (navigation_module_default()).line3
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (navigation_module_default()).menu
+            })
+        ]
+    }));
+}
+
+
+/***/ })
+
+};
+;
--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,57 +1,45 @@
-/*
- * ATTENTION: An "eval-source-map" devtool has been used.
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
+"use strict";
 (() => {
 var exports = {};
-exports.id = "pages/_app";
-exports.ids = ["pages/_app"];
+exports.id = 888;
+exports.ids = [888];
 exports.modules = {
 
-/***/ "./pages/_app.js":
-/*!***********************!*\
-  !*** ./pages/_app.js ***!
-  \***********************/
+/***/ 4694:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var next_script__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/script */ \"next/script\");\n/* harmony import */ var next_script__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_script__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _styles_globals_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../styles/globals.scss */ \"./styles/globals.scss\");\n/* harmony import */ var _styles_globals_scss__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_globals_scss__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\nfunction MyApp({ Component , pageProps  }) {\n    return(/*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n            ...pageProps\n        }, void 0, false, {\n            fileName: \"C:\\\\Users\\\\Admin\\\\Desktop\\\\проекты\\\\artelleContentfulBlog\\\\pages\\\\_app.js\",\n            lineNumber: 7,\n            columnNumber: 7\n        }, this)\n    }, void 0, false));\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0FBQWdDO0FBQ0Q7U0FFdEJDLEtBQUssQ0FBQyxDQUFDLENBQUNDLFNBQVMsR0FBRUMsU0FBUyxFQUFDLENBQUMsRUFBRSxDQUFDO0lBQ3hDLE1BQU07OEZBRURELFNBQVM7ZUFBS0MsU0FBUzs7Ozs7OztBQUc5QixDQUFDO0FBQ0QsaUVBQWVGLEtBQUssRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2FydGVsbGVjcmVhdGl2ZS8uL3BhZ2VzL19hcHAuanM/ZTBhZCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgU2NyaXB0IGZyb20gXCJuZXh0L3NjcmlwdFwiO1xyXG5pbXBvcnQgXCIuLi9zdHlsZXMvZ2xvYmFscy5zY3NzXCI7XHJcblxyXG5mdW5jdGlvbiBNeUFwcCh7IENvbXBvbmVudCwgcGFnZVByb3BzIH0pIHtcclxuICByZXR1cm4gKFxyXG4gICAgPD5cclxuICAgICAgPENvbXBvbmVudCB7Li4ucGFnZVByb3BzfSAvPlxyXG4gICAgPC8+XHJcbiAgKTtcclxufVxyXG5leHBvcnQgZGVmYXVsdCBNeUFwcDtcclxuIl0sIm5hbWVzIjpbIlNjcmlwdCIsIk15QXBwIiwiQ29tcG9uZW50IiwicGFnZVByb3BzIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _app)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+;// CONCATENATED MODULE: external "next/script"
+const script_namespaceObject = require("next/script");
+;// CONCATENATED MODULE: ./pages/_app.js
+
+
+
+function MyApp({ Component , pageProps  }) {
+    return(/*#__PURE__*/ jsx_runtime_.jsx(jsx_runtime_.Fragment, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+            ...pageProps
+        })
+    }));
+}
+/* harmony default export */ const _app = (MyApp);
+
 
 /***/ }),
 
-/***/ "./styles/globals.scss":
-/*!*****************************!*\
-  !*** ./styles/globals.scss ***!
-  \*****************************/
-/***/ (() => {
-
-
-
-/***/ }),
-
-/***/ "next/script":
-/*!******************************!*\
-  !*** external "next/script" ***!
-  \******************************/
+/***/ 997:
 /***/ ((module) => {
 
-"use strict";
-module.exports = require("next/script");
-
-/***/ }),
-
-/***/ "react/jsx-dev-runtime":
-/*!****************************************!*\
-  !*** external "react/jsx-dev-runtime" ***!
-  \****************************************/
-/***/ ((module) => {
-
-"use strict";
-module.exports = require("react/jsx-dev-runtime");
+module.exports = require("react/jsx-runtime");
 
 /***/ })
 
@@ -62,7 +50,7 @@ module.exports = require("react/jsx-dev-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__("./pages/_app.js"));
+var __webpack_exports__ = (__webpack_exec__(4694));
 module.exports = __webpack_exports__;
 
 })();
